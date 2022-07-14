3da8c9a6e2c0e5b10a7ede9584937a92d0302920
swastikgithub09 <89203698+swastikgithub09@users.noreply.github.com>
2022-07-14 21:12:17 +0530

    Add files via upload

---
+++ Leetcode/tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+for(int i=0; i <s.length(); i++)
+        // {
+        //     if(s[i] == 1)
+        //     {
+        //         count++;
+        //     }
+        // }
+        // cout<<count<<endl;
